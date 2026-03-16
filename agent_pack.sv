2aa888e7c21f768d1192c569fab14aadd03d3f3d
Mia <miamiretzky@gmail.com>
2026-03-16 16:09:40 +0200

    master driver

--- agent_pack.sv
+++ agent_pack.sv
@@ -14,8 +14,13 @@
 
 package agent_pack;
 
+    // parameters for driver slave
     localparam int unsigned LOW_RDY_PROBABILITY = 20;
-    localparam int unsigned HIGH_RDY_PROBABILITY = 20;
-    
+    localparam int unsigned HIGH_RDY_PROBABILITY = 80;
+
+    // parameters for driver master
+    localparam int unsigned LOW_VLD_PROBABILITY = 50;
+    localparam int unsigned HIGH_VLD_PROBABILITY = 50;
+
 
 endpackage : agent_pack
--- avalon_st_driver.sv
+++ avalon_st_driver.sv
@@ -44,14 +44,62 @@ class avalon_st_driver #(int DATA_WIDTH_IN_BYTES = 4);
             // randomize the rdy 
             std::randomize(rdy) with {
                 rdy dist {
-                    0 := LOW_RDY_PROBABILITY,
-                    1 := HIGH_RDY_PROBABILITY
+                    1'b0 := LOW_RDY_PROBABILITY,
+                    1'b1 := HIGH_RDY_PROBABILITY
                 };
             };
             vif.slave_cb.rdy <= rdy;
         end
     endtask
-    
+
+    task drive_master(input byte data[$]);
+        logic [DATA_WIDTH_IN_BYTES * $bits(byte) - 1 : 0] curr_word;
+        bit vld = 1'b0;
+        int words_amount = (data.size() + DATA_WIDTH_IN_BYTES - 1) / DATA_WIDTH_IN_BYTES;
+        int empty = DATA_WIDTH_IN_BYTES - (data.size() % DATA_WIDTH_IN_BYTES);
+        int byte_index = 0;
+        int word_num = 0;
+
+        while (word_num < words_amount) begin
+
+            // getting the current word
+            curr_word = '0;
+            for (int byte_in_word = 0; byte_in_word < DATA_WIDTH_IN_BYTES; byte_in_word++ )  begin
+                byte_index = byte_in_word * DATA_WIDTH_IN_BYTES + word_num;
+                if (byte_index < data.size()) begin
+                    curr_word[byte_in_word*$bits(byte) +: $bits(byte)] = data[byte_index];
+                end
+            end
+
+            // Changing the signals by the clock
+            @(vif.master_cb);
+
+            // Send HIGH sop with first word
+            vif.master_cb.sop <= (word_num == 0);
+
+            // Send empty and HIGH eop with last wor
+            vif.master_cb.eop   <= (word_num == words_amount);
+            vif.master_cb.empty <= (word_num == words_amount) ? empty : 0;
+
+            // Send the current word
+             vif.master_cb.data  <= curr_word;
+
+            // Randomize the vld, when its HIGH it will stays HIGH until transaction
+            if(!vld) begin
+                std::randomize(vld) with {
+                    1'b0 := LOW_VLD_PROBABILITY, 
+                    1'b1 := HIGH_VLD_PROBABILITY
+                }
+            end
+            vif.master_cb.valid <= vld;
+
+            // Move to next word if there was a transaction
+            if (vif.master_cb.rdy && master_cb.valid) begin
+                word++;
+                vld = 1'b0;
+            end
+        end
+    endtask
 endclass
 
 `endif // __AVALON_ST_DRIVER
